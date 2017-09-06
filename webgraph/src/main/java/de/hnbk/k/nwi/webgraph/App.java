d2c5bddf2177b5ad259617bbeb2b82d9cf4cac93
magnetotail <nils.wiechert@web.de>
2017-09-06 14:22:22 +0200

    Initialize maven for both Projects

---
+++ webgraph/src/main/java/de/hnbk/k/nwi/webgraph/App.java
@@ -0,0 +1,13 @@
+package de.hnbk.k.nwi.webgraph;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
